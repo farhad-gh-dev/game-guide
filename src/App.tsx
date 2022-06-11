7e79630f1d855c85915b5946337c4d0f1ef41504
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-06-11 20:58:27 +0430

    Add react-router to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import CategoryPage from "./Pages/Category";
 import { StyledApp } from "./Style/App.styled";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <StyledApp>
-      <CategoryPage />
-    </StyledApp>
+    <BrowserRouter>
+      <StyledApp>
+        <Routes>
+          <Route path="/" element={<CategoryPage />} />
+          <Route path="*" element={<h1>404 Page</h1>} />
+        </Routes>
+      </StyledApp>
+    </BrowserRouter>
   );
 }
 
