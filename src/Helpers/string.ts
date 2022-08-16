1e2e1359bfab1dccb6f56af228ccb783b35d580f
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-08-16 19:43:28 +0430

    Update category hook to handle 404 categories

--- src/Helpers/string.ts
+++ src/Helpers/string.ts
@@ -1,7 +1,7 @@
-export const replaceSpaceWithUnderscore = (s: string) => {
-  return s.replace(/ /g, "_");
+export const replaceSpaceWithUnderscore = (s?: string) => {
+  return s !== undefined ? s.replace(/ /g, "_") : "";
 };
 
-export const replaceUnderscoreWithSpace = (s: string) => {
-  return s.replace(/_/g, " ");
+export const replaceUnderscoreWithSpace = (s?: string) => {
+  return s !== undefined ? s?.replace(/_/g, " ") : "";
 };
--- src/Pages/Category/useCategory.ts
+++ src/Pages/Category/useCategory.ts
@@ -1,23 +1,28 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../Hooks/store";
 import { setActiveCategory, setLoadingStatus } from "../../Store/appSlice";
 import { replaceSpaceWithUnderscore } from "../../Helpers/string";
 
 export const useCategory = (userShoppingCartItems?: string[]) => {
   const dispatch = useAppDispatch();
-  const isLoading = useAppSelector((store) => store.app.loading);
-  const categoryItems = useAppSelector((store) => store.app.categoryItems);
   const slidersData = useAppSelector((store) => store.app.slidersData);
-  const offerItems = useAppSelector((store) => store.app.offerItems);
-  const collectionItems = useAppSelector((store) => store.app.collectionItems);
   const activeCategory = useAppSelector((store) => store.app.activeCategory);
 
   const urlParams = useParams();
+  const navigate = useNavigate();
   const targetCategory = urlParams.category?.toLowerCase();
-  const targetSliderItems = activeCategory
-    ? slidersData[replaceSpaceWithUnderscore(activeCategory)]
-    : [];
+  !Object.keys(slidersData).includes(
+    replaceSpaceWithUnderscore(activeCategory)
+  ) && navigate("/404");
+
+  const isLoading = useAppSelector((store) => store.app.loading);
+  const categoryItems = useAppSelector((store) => store.app.categoryItems);
+  const offerItems = useAppSelector((store) => store.app.offerItems);
+  const collectionItems = useAppSelector((store) => store.app.collectionItems);
+
+  const targetSliderItems =
+    slidersData[replaceSpaceWithUnderscore(activeCategory)];
 
   const formattedOfferItems = offerItems?.map((item) => {
     return {
