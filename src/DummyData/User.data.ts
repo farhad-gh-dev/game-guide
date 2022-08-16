7e2bed1242001dfadd160a36be9dc44adbce0b7e
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-08-16 19:43:53 +0430

    Change use profile image from url to local asset

--- src/DummyData/User.data.ts
+++ src/DummyData/User.data.ts
@@ -1,6 +1,7 @@
+import ProfileImage from "../Assets/Images/profile-pic.jpg";
+
 export const userProfile = {
   type: "pro",
   credit: 99000,
-  profileImageUrl:
-    "https://i.pinimg.com/736x/f0/34/58/f034583d34bd2bd5e52732a7ecc96669.jpg",
+  profileImageUrl: ProfileImage,
 };
