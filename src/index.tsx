c6c310a52259ec3d4755f3f29b4a9e7c4031c2ae
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-08-19 16:58:40 +0430

    Add sentry for performance monitoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./Store";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "game-guide-ui-kit";
 import { BrowserRouter } from "react-router-dom";
+
+Sentry.init({
+  dsn: "https://29e1b9a2274f4ef992099d2a06c8f289@o1103470.ingest.sentry.io/6668877",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
