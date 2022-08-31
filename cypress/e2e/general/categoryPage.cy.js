8da0db6b48b4f7ede605bb84c657b07fe3893b4e
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-08-31 23:32:46 +0430

    Add e2e test for shopping cart - Add and remove slider-item to cart - Add and remove offer-item to cart

--- cypress/e2e/general/categoryPage.cy.js
+++ cypress/e2e/general/categoryPage.cy.js
@@ -14,4 +14,32 @@ describe("category page", () => {
       "rgb(243, 61, 61)"
     );
   });
+
+  it("Add and remove slider item to shopping cart", () => {
+    // Add to cart
+    cy.get('[data-testid="slide-1"] button').click();
+    cy.get('button[aria-label="shopping cart"] span')
+      .should("be.visible")
+      .contains("1");
+
+    // Remove from cart
+    cy.get('[data-testid="slide-1"] button').click();
+    cy.get('button[aria-label="shopping cart"] span').should("not.exist");
+  });
+
+  it("Add and remove offer item to shopping cart", () => {
+    const targetOfferAddToCartButton = cy
+      .get('aside button[aria-label="Add To Cart"]')
+      .first();
+
+    // Add to cart
+    targetOfferAddToCartButton.click();
+    cy.get('button[aria-label="shopping cart"] span')
+      .should("be.visible")
+      .contains("1");
+
+    // Remove from cart
+    targetOfferAddToCartButton.click();
+    cy.get('button[aria-label="shopping cart"] span').should("not.exist");
+  });
 });
