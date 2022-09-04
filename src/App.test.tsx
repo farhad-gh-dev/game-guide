f126c1151db1cf0c84e7faff2f6066e28ab23a8c
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-09-04 22:42:57 +0430

    Fix app test bug caused by suspense

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,26 @@
-import { render, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Store";
 import { GlobalStyle } from "game-guide-ui-kit";
 import App from "./App";
+import { Suspense } from "react";
 
 afterEach(cleanup);
 
-test("renders learn react link", () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <App />
-      </BrowserRouter>
-    </Provider>
+test("renders learn react link", async () => {
+  render(
+    <Suspense>
+      <Provider store={store}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </Suspense>
   );
 
-  expect(getByText("Game")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.getByText("Special Offer")).toBeInTheDocument();
+  });
 });
